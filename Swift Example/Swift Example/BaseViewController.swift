7f95e25eacc3a36510eb1dcd169ca79b7c8b5ec8
Onur Akpolat <onur.akpolat@gmail.com>
2015-06-18 14:14:22 +0200

    stuff

--- Swift Example/Swift Example/BaseViewController.swift
+++ Swift Example/Swift Example/BaseViewController.swift
@@ -1,6 +1,6 @@
 //
 //  BaseViewController.swift
-//  Tapglue iOS Example
+//  Swift Example
 //
 //  Created by Martin Stemmle on 05/06/15.
 //  Copyright (c) 2015 Tapglue. All rights reserved.
--- Swift Example/Swift Example/CustomCell.swift
+++ Swift Example/Swift Example/CustomCell.swift
@@ -1,6 +1,6 @@
 //
 //  CustomCell.swift
-//  Tapglue iOS Example
+//  Swift Example
 //
 //  Created by Onur Akpolat on 09/06/15.
 //  Copyright (c) 2015 Tapglue. All rights reserved.
