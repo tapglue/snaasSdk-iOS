ae20d29bc9341e9a408611607d0cbe703a062dd8
Martin Stemmle <marste@msmix.de>
2015-09-29 15:46:03 +0200

    #85 Added test for images on TGUser

--- Tapglue Tests/Tapglue Tests/TGUserTests.m
+++ Tapglue Tests/Tapglue Tests/TGUserTests.m
@@ -34,14 +34,12 @@
 
 @implementation TGUserTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+#pragma mark - User general -
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)testImagesDictionaryIsNotNil {
+    TGUser *user = [[TGUser alloc] init];
+    expect(user.images).toNot.beNil();
+    expect(user.images).to.beKindOf([NSMutableDictionary class]);
 }
 
 #pragma mark - JSON to User -
