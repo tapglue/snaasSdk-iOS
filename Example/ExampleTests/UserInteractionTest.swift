dab38d364efb41702472fbbc79f2220ce074e14b
John Nilsen <nilsen340@gmail.com>
2016-07-12 16:29:57 +0200

    Add further acceptance tests for current user interactions

--- Example/ExampleTests/UserInteractionTest.swift
+++ Example/ExampleTests/UserInteractionTest.swift
@@ -13,12 +13,12 @@ import Nimble
 import RxBlocking
 
 class UserInteractionTest: XCTestCase {
-
-    let username = "userInteractionTestUser1"
-    let password = "userInteractionTestPassword"
+    
+    let username = "UserInteractionTestUser1"
+    let password = "UserInteractionTestPassword"
     let tapglue = RxTapglue(configuration: Configuration())
     var user = User()
-    
+
     override func setUp() {
         super.setUp()
         user.username = username
@@ -26,25 +26,35 @@ class UserInteractionTest: XCTestCase {
         
         do {
             user = try tapglue.createUser(user).toBlocking().first()!
+            user = try tapglue.loginUser(username, password: password).toBlocking().first()!
         } catch {
-            fail("failed to create user for integration tests")
+            fail("failed to create and login user for integration tests")
         }
     }
     
     override func tearDown() {
         super.tearDown()
         do {
+            try tapglue.loginUser(username, password: password).toBlocking().first()
             try tapglue.deleteCurrentUser().toBlocking().first()
         } catch {
-            fail("failed to delete user for integration tests")
+            fail("failed to login and delete user for integration tests")
         }
     }
-
-    func testUserLogin() {
-        do {
-            user = try tapglue.loginUser(username, password: password).toBlocking().first()!
-        } catch {
-            fail("failed to log in")
+    
+    func testLogout() {
+        var wasLoggedOut = false
+        _ = tapglue.logout().subscribeCompleted {
+            wasLoggedOut = true
         }
+        expect(wasLoggedOut).toEventually(beTrue())
+    }
+    
+    func testRefreshCurrentUser() {
+        var wasRefreshed = false
+        _ = tapglue.refreshCurrentUser().subscribeNext { user in
+            wasRefreshed = true
+        }
+        expect(wasRefreshed).toEventually(beTrue())
     }
 }
