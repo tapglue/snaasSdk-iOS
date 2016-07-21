f2f5096f65204785117297e583506ec2b15cc1f2
Onur Akpolat <onur.akpolat@gmail.com>
2016-07-21 11:15:21 +0200

    Add Implementation for Post CRUD

---
+++ Example/ExampleTests/PostTest.swift
@@ -0,0 +1,45 @@
+//
+//  PostTest.swift
+//  Example
+//
+//  Created by Onur Akpolat on 21/07/16.
+//  Copyright © 2016 Tapglue. All rights reserved.
+//
+
+import XCTest
+import Tapglue
+import RxSwift
+import Nimble
+import RxBlocking
+
+class PostTest: XCTestCase {
+
+    let username = "PostTestUser1"
+    let password = "PostTestPassword"
+    let tapglue = RxTapglue(configuration: Configuration())
+    var user = User()
+    
+    override func setUp() {
+        super.setUp()
+        user.username = username
+        user.password = password
+        
+        do {
+            user = try tapglue.createUser(user).toBlocking().first()!
+            user = try tapglue.loginUser(username, password: password).toBlocking().first()!
+        } catch {
+            fail("failed to create and login user for integration tests")
+        }
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        do {
+            try tapglue.loginUser(username, password: password).toBlocking().first()
+            try tapglue.deleteCurrentUser().toBlocking().first()
+        } catch {
+            fail("failed to login and delete user for integration tests")
+        }
+    }
+
+}
