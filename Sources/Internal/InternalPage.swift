36e0922cd060720c02191bef7748aff7cd8cecf2
John Nilsen <nilsen340@gmail.com>
2016-09-19 19:08:32 +0200

    RxPage proposal

---
+++ Sources/Internal/InternalPage.swift
@@ -0,0 +1,9 @@
+//
+//  InternalPage.swift
+//  Tapglue
+//
+//  Created by John Nilsen on 9/19/16.
+//  Copyright © 2016 Tapglue. All rights reserved.
+//
+
+import Foundation
--- Sources/RxPage.swift
+++ Sources/RxPage.swift
@@ -7,9 +7,25 @@
 //
 
 import RxSwift
+import ObjectMapper
 
-public protocol RxPage {
-    associatedtype T
-    var data: T {get}
-    var previous: Observable<Self> {get}
+class RxPage<T, F: NullableFeed> {
+    var data: [T] = [T]()
+    typealias Feed = Mappable
+    var previous: Observable<RxPage<T, F>> {
+        get {
+            return Http().execute(prevPointer!).map { (feed: F) in
+                return self.parseFeed(feed: feed)
+            }
+        }
+    }
+    
+    var prevPointer: NSMutableURLRequest?
+    var parseFeed: (feed: F) -> RxPage<T, F> = { feed in
+        return RxPage<T, F>()
+    }
+    
+ //   var previousPageGenerator: ((observable: Observable<[T]>) -> Observable<RxPage<T>>)?
+ //   var url: String?
+ //   var previousHash: String?
 }
